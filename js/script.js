d65d044acb1414a79795baac724224a2233d1911
lalitkumar <lalit.kumar@berylsystems.com>
2020-11-06 17:58:37 +0530

    add class on button click in js

--- js/script.js
+++ js/script.js
@@ -3,24 +3,28 @@ function addition(){
     a = Number(document.getElementById('input1').value);
     b = Number(document.getElementById('input2').value);
     document.getElementById('result').value = a + b; 
+    document.getElementById("result").classList.add("add");
 }
 function subtract(){
     var a,b;
     a = document.getElementById('input1').value;
     b = document.getElementById('input2').value;
     document.getElementById('result').value = a - b;
+    document.getElementById("result").classList.add("sub");
 }
 function multiply(){
     var a,b;
     a = document.getElementById('input1').value;
     b = document.getElementById('input2').value;
     document.getElementById('result').value = a * b;
+    document.getElementById("result").classList.add("mul");
 }
 function divide(){
     var a,b;
     a = document.getElementById('input1').value;
     b = document.getElementById('input2').value;
     document.getElementById('result').value = a / b;
+    document.getElementById("result").classList.add("div");
 }
 function reset(){
     document.getElementsByClassName('reset-class')[0].value = '';
@@ -32,6 +36,7 @@ function concat(){
     a = document.getElementById('input1').value;
     b = document.getElementById('input2').value;
     document.getElementById('result').value = `${a}${b}`; 
+    document.getElementById("result").classList.add("concat");
 }
 function swap(){
     var a,b;
@@ -41,4 +46,19 @@ function swap(){
     b = a ^ b;
     a = a ^ b;
     document.getElementById('result').value = `${a},${b}`; 
+    document.getElementById("result").classList.add("swap");
+}
+function reminder(){
+    var a,b;
+    a = document.getElementById('input1').value;
+    b = document.getElementById('input2').value;
+    document.getElementById('result').value = a % b;
+    document.getElementById("result").classList.add("reminder");
+}
+function doubles(){
+    var a,b;
+    a = document.getElementById('input1').value;
+    b = document.getElementById('input2').value;
+    document.getElementById('result').value = (a*2)*(b*2);
+    document.getElementById("result").classList.add("doubles");
 }
